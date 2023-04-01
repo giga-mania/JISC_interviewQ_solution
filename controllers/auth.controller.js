96de48549329bc0a98992a00777af7e5a76a75d3
giguchi <hil.giga@gmail.com>
2023-04-01 17:20:49 +0100

    switch to async/await from promise chain

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,91 +8,95 @@ const User = db.user
 const RefreshToken = db.refreshToken
 
 
-exports.register = (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10)
-    }).then((user) => {
+exports.register = async (req, res) => {
+
+    try {
+        const user = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10)
+        })
         if (user) {
             res.status(200).send({id: user.id, username: user.username, email: user.email})
         }
-    }).catch((err) => {
+    } catch (err) {
         res.status(500).send({message: err})
-    })
+    }
+
 }
 
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
     if (!(req.body.username && req.body.password)) {
         return res.status(404).send({
             message: "Auth credentials were not provided!"
         })
     }
 
-    User.findOne({where: {username: req.body.username}})
-        .then(async (user) => {
-            if (!user) {
-                return res.status(404).send({message: "User not found!"})
-            }
+    try {
+        const user = await User.findOne({where: {username: req.body.username}})
+        if (!user) {
+            return res.status(404).send({message: "User not found!"})
+        }
 
-            let passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
+        let passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
 
-            if (!passwordIsValid) {
-                return res.status(401).send({
-                    message: "Invalid password!"
-                })
-            }
-
-            let token = jwt.sign({id: user.id}, config.privateKey, {
-                expiresIn: config.jwtExpiration
+        if (!passwordIsValid) {
+            return res.status(401).send({
+                message: "Invalid password!"
             })
-            let refreshToken = await RefreshToken.createToken(user)
+        }
+
+        let token = jwt.sign({id: user.id}, config.privateKey, {
+            expiresIn: config.jwtExpiration
+        })
+        let refreshToken = await RefreshToken.createToken(user)
 
 
-            res.status(200).send({
-                id: user.id,
-                username: user.username,
-                email: user.email,
-                accessToken: token,
-                refreshToken
-            })
+        return res.status(200).send({
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            accessToken: token,
+            refreshToken
         })
-        .catch((err) => {
-            console.log(err)
-            res.status(500).send(err)
-        })
+    } catch (err) {
+        res.status(500).send({message: err.message})
+    }
 }
 
 
-exports.refresh = (req, res) => {
+exports.refresh = async (req, res) => {
     const requestToken = req.body.refreshToken
 
     if (!requestToken) {
         return res.status(403).send({message: "Refresh token is required!"})
     }
 
-    RefreshToken.findOne({where: {token: requestToken}})
-        .then((token) => {
-            if (!token) {
-                return res.status(403).send({message: "Refresh token not in database!"})
-            }
+    try {
+        const token = await RefreshToken.findOne({where: {token: requestToken}})
+        if (!token) {
+            return res.status(403).send({message: "Refresh token not in database!"})
+        }
 
-            if (RefreshToken.verifyExpiration(token)) {
-                return res.status(403).send({message: "Refresh token is expired. Login in again!"})
-            }
+        if (RefreshToken.verifyExpiration(token)) {
+            return res.status(403).send({message: "Refresh token is expired. Login in again!"})
+        }
 
-            token.getUser()
-                .then(async (user) => {
-                    let newAccessToken = jwt.sign({id: user.id}, config.privateKey, {
-                        expiresIn: config.jwtExpiration
-                    })
-
-                    res.status(200).send({
-                        accessToken: newAccessToken,
-                        refreshToken: await RefreshToken.createToken(user)
-                    })
-                })
-
+        const user = await token.getUser()
+        let newAccessToken = jwt.sign({id: user.id}, config.privateKey, {
+            expiresIn: config.jwtExpiration
         })
+
+        return res.status(200).send({
+            accessToken: newAccessToken,
+            refreshToken: await RefreshToken.createToken(user)
+        })
+
+    } catch (err) {
+        res.stat(500).send({message: err})
+    }
 }
+
+
+
--- middleware/verifySignUp.js
+++ middleware/verifySignUp.js
@@ -2,24 +2,21 @@ const db = require("../models")
 const User = db.user
 
 
-exports.checkDuplicateUsernameOrEmail = (req, res, next) => {
-    User.findOne({where: {username: req.body.username}})
-        .then((user) => {
-            if (user) {
-                return res.status(400).send({
-                    message: "Failed! Username is already in use!"
-                })
-            }
+exports.checkDuplicateUsernameOrEmail = async (req, res, next) => {
 
-            User.findOne({where: {email: req.body.email}})
-                .then((user) => {
-                    if (user) {
-                        return res.status(400).send({
-                            message: "Failed! Email is already in use!"
-                        })
-                    }
-
-                    next()
-                })
+    const username = await User.findOne({where: {username: req.body.username}})
+    if (username) {
+        return res.status(400).send({
+            message: "Failed! Username is already in use!"
         })
+    }
+
+    const email = await User.findOne({where: {email: req.body.email}})
+    if (email) {
+        return res.status(400).send({
+            message: "Failed! Email is already in use!"
+        })
+    }
+
+    next()
 }
