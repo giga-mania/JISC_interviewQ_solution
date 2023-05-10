e113566086e13542c1d28ee06ee157b3ac7771f4
giga-mania <hil.giga@gmail.com>
2023-05-10 13:33:37 +0100

    Add logger on Sequelize connection

--- models/index.js
+++ models/index.js
@@ -7,6 +7,14 @@ const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
     dialect: config.DIALECT,
 })
 
+
+sequelize.authenticate().then(() => {
+    console.log('Sequelize connection has been established successfully!');
+}).catch(error => {
+    console.error('Unable to connect to the database:', error);
+})
+
+
 const db = {}
 
 db.Sequelize = Sequelize
@@ -23,7 +31,6 @@ db.question.belongsToMany(db.trainer, {through: db.trainerAnswer})
 
 db.user.belongsToMany(db.question, {through: db.userAnswer})
 db.question.belongsToMany(db.user, {through: db.userAnswer})
-
 
 
 module.exports = db
