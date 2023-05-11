cf26dfd1cc698ef54d004728cafae375f332d024
giga-mania <hil.giga@gmail.com>
2023-05-11 21:33:04 +0100

    Migrate AuthContext to TypeScript

--- client/src/context/authContext.tsx
+++ client/src/context/authContext.tsx
@@ -2,30 +2,49 @@ import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 
-const initialState = {
-    user: localStorage.getItem("user"),
-    setAuthUser: () => {
-    },
-    logOut: () => {
-    }
+type User = {
+    id: number,
+    username: string,
+    email: string,
+    accessToken: string,
+    hasAnswered: boolean,
+    isAdmin: boolean
 }
+
+type AuthContextType = {
+    user: User | null,
+    setAuthUser: (user: User) => void,
+    logOut: () => void
+}
+
+type AuthContextProviderProps = {
+    children: React.ReactNode
+}
+
+
+const initialState: AuthContextType = {
+    user: JSON.parse(localStorage.getItem("user")!),
+    setAuthUser: () => {},
+    logOut: () => {}
+}
+
+
 const AuthContext = React.createContext(initialState)
 AuthContext.displayName = "AuthContext"
 
-const AuthContextProvider = ({children}) => {
-    const [user, setUser] = useState("")
+const AuthContextProvider = ({children}: AuthContextProviderProps) => {
+    const [user, setUser] = useState<User | null>(null)
     const navigate = useNavigate()
 
     useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("user"))
+        const user = JSON.parse(localStorage.getItem("user")!)
         if (user) {
             setAuthUser(user)
             navigate("/answer-questions")
         }
     }, [])
-
-
-    const setAuthUser = (user) => {
+    
+    const setAuthUser = (user: User) => {
         setUser(user)
         localStorage.setItem("user", JSON.stringify(user))
     }
@@ -34,7 +53,6 @@ const AuthContextProvider = ({children}) => {
         setUser(null)
         localStorage.removeItem("user")
     }
-
 
     const value = {
         user,
